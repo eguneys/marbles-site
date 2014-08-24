9765f01f522d0728602ffb1c5fd8a7c1fd7c7189
eguneys <eguneys@hotmail.com>
2014-08-24 20:55:46 +0300

    robots.txt route added, og, twitter meta headers added

--- app/server/routes/public.js
+++ app/server/routes/public.js
@@ -10,6 +10,10 @@ module.exports = function(router) {
     router.use(morgan("dev", {}));
     router.use('/public', express.static(path.join(__dirname, '../../../', 'public'), { maxAge: 24 * 60 * 60 * 1000 }));
 
+    router.use('/robots.txt', function(req, res) {
+        res.sendfile(path.join(__dirname, '../../../', 'public', 'assets', 'robots.txt'));
+    });
+    
     router.get('/', function(req, res) {
         res.render('index');
     });
